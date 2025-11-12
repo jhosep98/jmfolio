09ec8905343a86f1ea43cbd995f9a90652d3844c
jhosep98 <jhosep149@hotmail.com>
2025-11-12 19:44:00 -0300

    feat: setup vitest and example

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function sum(a: number, b: number) {
+  return a + b
+}
